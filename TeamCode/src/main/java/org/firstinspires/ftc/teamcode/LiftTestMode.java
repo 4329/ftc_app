d752a396466042dcc903f90728b574af770ca96a
Matt Zilz <mzilz1432@outlook.com>
2018-10-16 18:46:29 -0500

    Working up.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTestMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTestMode.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 @TeleOp(name="LiftTestMode", group="Iterative Opmode")
-public class LiftTestMode extends OpMode{
+public class LiftTestMode extends OpMode {
 
     private DcMotor liftMotor = null;
     private DigitalChannel digitalChannelUp = null;
@@ -17,8 +17,8 @@ public class LiftTestMode extends OpMode{
 
     @Override
     public void init() {
-        liftMotor = hardwareMap.get(DcMotor.class,"LiftMotor");
-        digitalChannelUp = hardwareMap.get(DigitalChannel.class,"DigitalChannelUp");
+        liftMotor = hardwareMap.get(DcMotor.class, "LiftMotor");
+        digitalChannelUp = hardwareMap.get(DigitalChannel.class, "DigitalChannelUp");
         digitalChannelDown = hardwareMap.get(DigitalChannel.class, "DigitalChannelDown");
 
         digitalChannelUp.setMode(DigitalChannel.Mode.INPUT);
@@ -26,36 +26,21 @@ public class LiftTestMode extends OpMode{
 
         liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        if(!digitalChannelDown.getState()){
+        if (!digitalChannelDown.getState()) {
             liftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         }
 
-        telemetry.addData("DigitalChannelDown",digitalChannelDown.getState());
+        telemetry.addData("DigitalChannelDown", digitalChannelDown.getState());
 
     }
 
     @Override
     public void loop() {
-        if(gamepad2.b){
-            liftPowered = true;
+        if (gamepad2.b) {
+            liftMotor.setPower(-1);
         }
-
-        if (liftPowered) {
-            liftMotor.setPower(1);
-        }
-
-        if (!digitalChannelUp.getState() || !digitalChannelDown.getState()){
+        if (!digitalChannelUp.getState()){
             liftMotor.setPower(0);
-            reverseDirection();
-            liftPowered = false;
-        }
-    }
-
-    private void reverseDirection() {
-        if (liftMotor.getDirection() == DcMotorSimple.Direction.FORWARD) {
-            liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        } else {
-            liftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         }
     }
 }
